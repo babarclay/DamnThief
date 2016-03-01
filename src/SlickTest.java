0d5018f28ea274ca2492e2a89458562b92e7b459
Bradford Barclay <babararclay.bsu.edu>
2016-03-01 10:19:37 -0500

    Added prototype art for room into new folder "resources".
    Project now displays dungeonTiles.png to the screen at 2x scale. Note
    that use of Image class is inappropriate for most game-art. We will
    likely only use this class for splash screens and the like.

--- src/SlickTest.java
+++ src/SlickTest.java
@@ -1,35 +1,42 @@
 
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SpriteSheet;
 
 public class SlickTest extends BasicGame {
+	SpriteSheet dungeonTiles;
+	
+	public SlickTest() {
+		super("SimpleTest");
+	}
 
-    public SlickTest() {
-        super("SimpleTest");
-    }
-    
-    @Override
-    public void init(GameContainer container) throws SlickException {}
+	@Override
+	public void init(GameContainer container) throws SlickException {
+		dungeonTiles = new SpriteSheet("resources/dungeonTiles.png",304,208);
+	}
 
-    @Override
-    public void update(GameContainer container, int delta)
-            throws SlickException {}
+	@Override
+	public void update(GameContainer container, int delta)
+			throws SlickException {
+	}
 
-    @Override
-    public void render(GameContainer container, Graphics g)
-            throws SlickException {
-        g.drawString("Hello, Slick world!", 0, 100);
-    }
+	@Override
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		g.drawString("Hello, Slick world!", 0, 100);
+		dungeonTiles.draw(0, 0,2);
+	}
 
-    public static void main(String[] args) {
-        try {
-            AppGameContainer app = new AppGameContainer(new SlickTest());
-            app.start();
-        } catch (SlickException e) {
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		try {
+			AppGameContainer app = new AppGameContainer(new SlickTest());
+			app.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
 }
