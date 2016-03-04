e6a202ee6ccb57596ef804b7ddd7ebfaf5bc576e
Bradford Barclay <babararclay.bsu.edu>
2016-03-03 21:30:16 -0500

    Implemented TiledMap obj to render custom tmx file
    included tmx file and spritesheet the tmx is based on into resources
    folder of project.

--- src/SlickTest.java
+++ src/SlickTest.java
@@ -3,20 +3,22 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
+import org.newdawn.slick.tiled.TiledMap;
 
 public class SlickTest extends BasicGame {
 	SpriteSheet dungeonTiles;
-	
+	TiledMap prototypeRoom;
+
 	public SlickTest() {
 		super("SimpleTest");
 	}
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
-		dungeonTiles = new SpriteSheet("resources/dungeonTiles.png",304,208);
+		dungeonTiles = new SpriteSheet("resources/sheet.png", 304, 208);
+		prototypeRoom = new TiledMap("resources/prototypeRoom.tmx");
 	}
 
 	@Override
@@ -28,7 +30,8 @@ public class SlickTest extends BasicGame {
 	public void render(GameContainer container, Graphics g)
 			throws SlickException {
 		g.drawString("Hello, Slick world!", 0, 100);
-		dungeonTiles.draw(0, 0,2);
+		dungeonTiles.draw(0, 0, 2);
+		prototypeRoom.render(0,0);
 	}
 
 	public static void main(String[] args) {
